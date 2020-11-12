3233c509d2f722f37c407b3a45a7ad9f38cbd33e
Myung-gwan Kim <cartopia95@naver.com>
2020-11-12 09:24:49 +0900

    add - 에코서버 등록

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/GreatLaboratory/simplify-petition-crawling-server/controller"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	e := echo.New()
+	e.Static("/static", "static")
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	}))
+
+	e.GET("/wordCloud/:category", controller.WordCloudController)
+	e.GET("/topFivePosts/:category", controller.TopFiveController)
+	e.GET("/summary/:petitionID", controller.SummaryController)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
