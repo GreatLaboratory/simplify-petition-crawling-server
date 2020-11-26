00815f7257f049e58591c138da390da907c32811
Myung-gwan Kim <cartopia95@naver.com>
2020-11-26 20:23:35 +0900

    add - cors 설정 전부 허용

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "method=${method}, uri=${uri}, status=${status}\n",
 	}))
+	e.Use(middleware.CORS())
 
 	e.GET("/wordCloud/:category", controller.WordCloudController)
 	e.GET("/topFivePosts/:category", controller.TopFiveController)
